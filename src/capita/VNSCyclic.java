118ee9230581f56ec9fd9060cd28855718cd83aa
JulieWouters <Julie.wouters@student.kuleuven.be>
2016-03-24 17:50:51 +0100

    equals and cyclic

--- src/capita/VNSCyclic.java
+++ src/capita/VNSCyclic.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class VNSCyclic extends VariableNeighborhoodSearch {
 	public int timespan;
@@ -19,23 +20,102 @@ public class VNSCyclic extends VariableNeighborhoodSearch {
 
 	@Override
 	public ArrayList<int[]> exploreNeighborhood(ArrayList<int[]> x, int l) {
-		// TODO Auto-generated method stub
+		ArrayList<ArrayList<int[]>> allNeighbours;
+		if (l < 3) {
+			allNeighbours = getAllKSwapNeighbours(x, l);
+		} else if (l == 3) {
+			allNeighbours = getAllAddDropNeighbours(x);
+		} else {
+			allNeighbours = getAllKSwapNeighbours(x, l);
+		}
+		
+		ArrayList<int[]> bestNeighbour = new ArrayList<int[]>();
+		int cost = Integer.MAX_VALUE;
+		for(ArrayList<int[]> neighbour : allNeighbours) {
+			int newCost = getCost(neighbour);
+			if(newCost < cost) {
+				cost = newCost;
+				bestNeighbour = neighbour;
+			}
+		}
+		
+		return bestNeighbour;
+	}
+
+	private ArrayList<ArrayList<int[]>> getAllAddDropNeighbours(ArrayList<int[]> x) {
+		ArrayList<ArrayList<int[]>> allNeighbours = new ArrayList<ArrayList<int[]>>();
+		Random generator = new Random();
+		for(int i = 0; i < x.size() + 1; i++) {
+			if(i < x.size()) {
+				x.remove(i);
+				allNeighbours.add(x);
+			} else {
+				int[] permutation = cycle.clone();
+				permutate(permutation, generator.nextInt(timespan-1)+1);
+				x.add(permutation.clone());
+				allNeighbours.add(x);
+			}
+		}
+		
+		return allNeighbours;
+	}
+
+	private ArrayList<ArrayList<int[]>> getAllKSwapNeighbours(ArrayList<int[]> x, int l) {
+
 		return null;
 	}
 
 	@Override
 	public ArrayList<int[]> shake(ArrayList<int[]> x, int k) {
-		// TODO Auto-generated method stub
-		return null;
+		if (k < 3) {
+			return getRandomKSwapNeighbour(x, k);
+		} else if (k == 3) {
+			return getRandomAddDropNeighbour(x);
+		} else {
+			return getRandomKSwapNeighbour(x, k-1);
+		}
+	}
+
+	private ArrayList<int[]> getRandomAddDropNeighbour(ArrayList<int[]> x) {
+		Random generator = new Random(); 
+		int index = generator.nextInt(x.size() + 1);
+		if(index < x.size()) {
+			x.remove(index);
+			return x;
+		} else {
+			int[] permutation = cycle.clone();
+			permutate(permutation, generator.nextInt(timespan-1)+1);
+			x.add(permutation.clone());
+			return x;
+		}
+	}
+
+	private ArrayList<int[]> getRandomKSwapNeighbour(ArrayList<int[]> x, int k) {
+		Random generator = new Random();
+		int[] permutation = cycle.clone();
+		ArrayList<int[]> randomNeighbour = x;
+		int n = k;
+		while(n > 0){
+			int rowIndex = generator.nextInt(x.size()-(k-n));
+			randomNeighbour.remove(rowIndex);
+			n--;
+		}
+		n = k;
+		while(n > 0){
+			permutate(permutation, generator.nextInt(timespan-1)+1);
+			randomNeighbour.add(permutation.clone());
+			n--;
+		}
+		return randomNeighbour;
 	}
 
 	@Override
 	public ArrayList<int[]> createInitialSolution() {
 		ArrayList<int[]> solution = new ArrayList<int[]>();
-		int[] perm = cycle;
+		int[] perm = cycle.clone();
 		for(int i = 0; i < timespan; i++){
 			while(perm[i] == 0){
-				permutate(perm);
+				permutate(perm,1);
 			}
 			while(!demandSatisfied(solution,i)){
 				solution.add(perm.clone());
@@ -55,12 +135,15 @@ public class VNSCyclic extends VariableNeighborhoodSearch {
 		return nbEmpl >= demand[i];
 	}
 
-	private void permutate(int[] perm) {
-		int temp = perm[0];
-		for(int i = timespan - 1; i > 0; i--){
-			perm[(i+1) % timespan] = perm[i];
+	private void permutate(int[] perm, int j) {
+		while(j > 0){
+			int temp = perm[0];
+			for(int i = timespan - 1; i > 0; i--){
+				perm[(i+1) % timespan] = perm[i];
+			}
+			perm[1]=temp;
+			j--;
 		}
-		perm[1]=temp;
 	}
 
 	@Override
